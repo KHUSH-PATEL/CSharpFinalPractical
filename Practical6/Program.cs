5abe4f34783ac0dd48329c3ee336ae4a6e3d90bf
Khush <khush@simformsolutions.com>
2023-04-12 11:36:37 +0530

    Practical 6 of C#

---
+++ Practical6/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practical6;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ProcessBusinessLogic processBusinessLogic = new ProcessBusinessLogic();
+        processBusinessLogic.ProcessCompleted += bl_ProcessCompleted;
+        processBusinessLogic.StartProcess();
+    }
+
+    /// <summary>
+    /// When StartProcess will complete bl_ProcessCompleted method will called.
+    /// </summary>
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Method Invoked");
+    }
+}
