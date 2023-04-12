ab8fa1b6150ca92b0581d96a10bd2c0a8fe30fba
Khush <khush@simformsolutions.com>
2023-04-12 11:27:40 +0530

    Practical 3 of C#

---
+++ Practical3/Polymorphism/Duck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3.Polymorphism
+{
+    internal class Duck : Bird
+    {
+        /// <summary>
+        /// This method will override Bird.Voice()
+        /// </summary>
+        public override void Voice()
+        {
+            Console.WriteLine("Quack Quack");
+        }
+    }
+}
---
+++ Practical3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Practical3.Abstraction;
+using Practical3.Inheritance;
+using Practical3.Polymorphism;
+
+namespace Practical3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Inheritance
+            Team team = new Team(" CSK");
+            string teamDetails = team.printInfo();
+            Console.WriteLine(teamDetails);
+
+            //Polymorphism
+            Bird myBird = new Bird();
+            Duck myDuck = new Duck();
+            myBird.Voice();
+            myDuck.Voice();
+
+            //Abstraction
+            Laptop laptop = new Laptop();
+            laptop.Brand = "Lenovo";
+            laptop.Model = "Legion Y540 i7, 9th gen, 256GB SSD, 1TB HDD";
+            laptop.LaptopDetails();
+            //laptop.MotherBoardInfo();
+        }
+    }
+}
